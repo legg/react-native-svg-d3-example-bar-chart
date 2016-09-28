d97dc0d329e03fef6f3acce4b2d133b1a4cae364
Maximus <maxlegg@gmail.com>
2016-09-28 23:05:12 +0100

    starting point

---
+++ app/root.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+class App extends Component {
+    render() {
+        return (
+            <View>
+                <Text>We got it</Text>
+            </View>
+        )
+    }
+}
+
+export default App
