7aaed34fdeae6c3d1bd38aaee69eeefbe0fbd9ff
Maximus <maxlegg@gmail.com>
2016-10-02 19:37:05 +0100

    Bar chart axes added, downgraded react-native to 0.33 to solve svg bug

--- android/app/src/main/java/com/d3native/MainActivity.java
+++ android/app/src/main/java/com/d3native/MainActivity.java
@@ -1,6 +1,7 @@
 package com.d3native;
 
 import com.facebook.react.ReactActivity;
+import com.horcrux.svg.RNSvgPackage;
 
 public class MainActivity extends ReactActivity {
 
--- app/root.js
+++ app/root.js
@@ -1,13 +1,141 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import {View, Dimensions} from 'react-native'
+
+import Svg, {
+    Circle,
+    Ellipse,
+    G,
+    LinearGradient,
+    RadialGradient,
+    Line,
+    Path,
+    Polygon,
+    Polyline,
+    Rect,
+    Symbol,
+    Text,
+    Use,
+    Defs,
+    Stop
+} from 'react-native-svg'
+
+// d3 lib
+import {
+    scaleBand,
+    scaleLinear
+} from 'd3-scale'
+
+import {
+    max,
+    mean,
+    ticks
+} from 'd3-array'
+
+import {
+    line
+} from 'd3-shape'
+
+import {
+    path
+} from 'd3-path'
+
+// create the barchart (http://bl.ocks.org/mbostock/3885304)
+const data = [
+    {frequency: 2, letter: 'a'},
+    {frequency: 5, letter: 'b'},
+    {frequency: 4, letter: 'c'},
+    {frequency: 1, letter: 'd'},
+]
 
 class App extends Component {
     render() {
         return (
             <View>
-                <Text>We got it</Text>
+                <BarChart />
             </View>
         )
+    }
+}
+
+class BarChart extends Component {
+    render() {
+        const screen = Dimensions.get('window')
+        const margin = {top: 50, right: 25, bottom: 200, left: 25}
+        const width = screen.width - margin.left - margin.right
+        const height = screen.height - margin.top - margin.bottom
+        const x = scaleBand()
+            .rangeRound([0, width])
+            .padding(0.1)
+            .domain(data.map(d => d.letter))
+        const maxFrequency = max(data, d => d.frequency)
+        const y = scaleLinear()
+            .rangeRound([height, 0])
+            .domain([0, maxFrequency])
+
+        const labelDx = (x(data[1].letter) - x(data[0].letter)) / 2
+
+        const bottomAxis = [x('a') - labelDx, x('d') + labelDx]
+        const bottomAxisD = line()
+            .x(d => d + labelDx)
+            .y(() => 0)
+            (bottomAxis)
+
+        const leftAxis = ticks(0, maxFrequency, 5)
+        const leftAxisD = line()
+            .x(() => bottomAxis[0] + labelDx)
+            .y(d => y(d) - height)
+            (leftAxis)
+
+        const notch = 5
+        const labelDistance = 9
+
+        const black = '#000'
+
+        const svg = (
+            <Svg width={screen.width} height={screen.height}>
+                <G translate={margin.left + "," + margin.top}>
+                    <G translate={"0," + height}
+                       fill="none"
+                       font-size="10"
+                       font-family="sans-serif"
+                       text-anchor="middle">
+                        <G key={-1}>
+                            <Path stroke={black} d={bottomAxisD} key="-1"/>
+                            {
+                                data.map((d, i) => (
+                                    <G opacity="1" key={i + 1} translate={x(d.letter) + labelDx + ",0"}>
+                                        <Line stroke={black} y2={notch}/>
+                                        <Text fill={black} y={labelDistance} dx={0}>
+                                            {d.letter}
+                                        </Text>
+                                    </G>
+                                ))
+                            }
+                        </G>
+                        <G key={-2}>
+                            <Path stroke={black} d={leftAxisD} key="-1"/>
+                            {
+                                leftAxis.map((d, i) => (
+                                    <G opacity="1" key={i + 1} translate={"0," + (y(d) - height)}>
+                                        <Line stroke={black} x1={notch} x2={labelDistance}/>
+                                        <Text fill={black} x={-labelDistance} y={-notch}>
+                                            {d}
+                                        </Text>
+                                    </G>
+                                ))
+                            }
+                        </G>
+                        {
+                            data.map((d, i) => {
+                                return <Rect key={i} />
+                            })
+                        }
+                    </G>
+                </G>
+            </Svg>
+        )
+
+        return svg;
     }
 }
 
